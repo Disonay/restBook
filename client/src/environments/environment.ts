bd9f2939e30cf2284745842b38902e9b0fe50323
Sergei <mpochta8@gmail.com>
2022-10-16 01:49:52 +0300

    Add cas
    Add cas

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,10 @@
 export const environment = {
   production: false,
   development: true,
-  environmentName: "DEV",
-  booksBaseURL: "http://localhost:5050/api/books/",
-  authorsBaseURL: "http://localhost:5050/api/authors/"
+  environmentName: 'DEV',
+  booksBaseURL: 'http://localhost:5050/api/books/',
+  authorsBaseURL: 'http://localhost:5050/api/authors/',
+  cas_url: 'https://localhost:8443/cas',
+  app_url: 'http://localhost:4200',
+  cas_validate_url: 'https://localhost:8443/cas/validate'
 };
